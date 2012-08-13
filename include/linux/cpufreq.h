6730fb7207e18461bdf797e00451168e0dc9c9ed
ktoonsez <ktoonsez@gmail.com>
2012-08-12 23:48:23 -0700

    Updated OC/UC/UV/OV interface

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -354,10 +354,9 @@ static inline unsigned int cpufreq_quick_get(unsigned int cpu)
 
 #define LOW_MAX_FREQ_LIMIT 1188000
 
-#define MIN_FREQ_LIMIT 192000 
+static int GLOBALKT_MIN_FREQ_LIMIT = 384000;
 //#define MAX_FREQ_LIMIT 1512000
-#define MAX_FREQ_LIMIT 1890000
-#define MAX_FREQ_LIMIT_STARTUP 1512000
+static int GLOBALKT_MAX_FREQ_LIMIT = 1512000;
 
 enum {
 	SET_MIN = 0,
