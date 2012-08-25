b896417e40f1219df69ea911a52a16a5a40993f5
ktoonsez <ktoonsez@gmail.com>
2012-08-24 19:52:31 -0700

    update enable-oc.sh

--- enable-oc.sh
+++ enable-oc.sh
@@ -1,7 +1,8 @@
 #!/system/bin/sh
 
 #Set governor items
-echo 1 > /sys/devices/system/cpu/cpufreq/ktoonsez/enable_oc;
-echo 2106000 > /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq;
+echo 384000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq;
+echo 1512000 > /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq;
+echo 1512000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq;
 
 echo $(date) END of post-init.sh
