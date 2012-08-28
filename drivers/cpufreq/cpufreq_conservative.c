f25bfa6588d5e9defec2d6b1890f85095d40ac84
ktoonsez <ktoonsez@gmail.com>
2012-08-28 16:36:53 -0700

    gov: lower min_sampling_rate in conservative gov.

--- drivers/cpufreq/cpufreq_conservative.c
+++ drivers/cpufreq/cpufreq_conservative.c
@@ -532,12 +532,12 @@ static int cpufreq_governor_dbs(struct cpufreq_policy *policy,
 			 * governor, thus we are bound to jiffes/HZ
 			 */
 			min_sampling_rate =
-				MIN_SAMPLING_RATE_RATIO * jiffies_to_usecs(10);
+				(MIN_SAMPLING_RATE_RATIO * jiffies_to_usecs(10)) / 20;
 			/* Bring kernel and HW constraints together */
 			min_sampling_rate = max(min_sampling_rate,
 					MIN_LATENCY_MULTIPLIER * latency);
 			dbs_tuners_ins.sampling_rate =
-				max(min_sampling_rate,
+				max((min_sampling_rate * 20),
 				    latency * LATENCY_MULTIPLIER);
 
 			cpufreq_register_notifier(
