6a5cbc38fe87df14494c33a4376905670e9ba359
ktoonsez <ktoonsez@gmail.com>
2012-09-05 00:25:07 -0700

    Revert "mm/vmscan.c: remove aborted_reclaim, compaction_ready & shrink_zones.  according to user klothius from XDA, those functions were the cause for conflict with Android LMK which leads to improper memory reclaimation which would eventually result in laggy performance due to low memory issue."
    This reverts commit 9f75addf53d9ab9e4929b5289cb4b610d3839b55.

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -2076,7 +2076,6 @@ restart:
 	throttle_vm_writeout(sc->gfp_mask);
 }
 
-#if 0
 /* Returns true if compaction should go ahead for a high-order request */
 static inline bool compaction_ready(struct zone *zone, struct scan_control *sc)
 {
@@ -2191,7 +2190,6 @@ static bool shrink_zones(int priority, struct zonelist *zonelist,
 
 	return aborted_reclaim;
 }
-#endif
 
 static bool zone_reclaimable(struct zone *zone)
 {
@@ -2244,7 +2242,7 @@ static unsigned long do_try_to_free_pages(struct zonelist *zonelist,
 	struct zoneref *z;
 	struct zone *zone;
 	unsigned long writeback_threshold;
-	//bool aborted_reclaim;
+	bool aborted_reclaim;
 
 	delayacct_freepages_start();
 
@@ -2255,7 +2253,7 @@ static unsigned long do_try_to_free_pages(struct zonelist *zonelist,
 		sc->nr_scanned = 0;
 		if (!priority)
 			disable_swap_token(sc->mem_cgroup);
-		//aborted_reclaim = shrink_zones(priority, zonelist, sc);
+		aborted_reclaim = shrink_zones(priority, zonelist, sc);
 
 		/*
 		 * Don't shrink slabs when reclaiming memory from
@@ -2321,8 +2319,8 @@ out:
 		return 0;
 
 	/* Aborted reclaim to try compaction? don't OOM, then */
-	//if (aborted_reclaim)
-	//	return 1;
+	if (aborted_reclaim)
+		return 1;
 
 	/* top priority shrink_zones still had more to do? don't OOM, then */
 	if (scanning_global_lru(sc) && !all_unreclaimable(zonelist, sc))
