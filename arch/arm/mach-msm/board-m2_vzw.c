0d83c948f6f165a542715e289868d14ebd62e253
ktoonsez <ktoonsez@gmail.com>
2012-09-12 07:31:14 -0700

    fast charge: add missing vars to spr and vzw

--- arch/arm/mach-msm/board-m2_vzw.c
+++ arch/arm/mach-msm/board-m2_vzw.c
@@ -188,6 +188,7 @@ static unsigned char hdmi_is_primary = 1;
 #else
 static unsigned char hdmi_is_primary;
 #endif
+extern int force_fast_charge;
 
 static struct platform_device msm_fm_platform_init = {
 	.name = "iris_fm",
@@ -1622,7 +1623,8 @@ int msm8960_get_cable_type(void)
 #endif
 			break;
 		case CABLE_TYPE_USB:
-			fsa9485_usb_cb(1);
+			if (!force_fast_charge) fsa9485_usb_cb(1);
+				else fsa9485_charger_cb(1);
 			break;
 		case CABLE_TYPE_AC:
 			fsa9485_charger_cb(1);
