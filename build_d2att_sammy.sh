72d82982e17f96f47860fdb69d894c67af43da7e
ktoonsez <ktoonsez@gmail.com>
2012-09-29 17:02:47 -0700

    Update build scripts to help build all 8 million kernels now

--- build_d2att_sammy.sh
+++ build_d2att_sammy.sh
@@ -53,4 +53,11 @@ cp arch/arm/boot/zImage $PACKAGEDIR/zImage
 echo "Make boot.img"
 ./mkbootfs $INITRAMFS_DEST | gzip > $PACKAGEDIR/ramdisk.gz
 ./mkbootimg --cmdline 'console = null androidboot.hardware=qcom user_debug = 31' --kernel $PACKAGEDIR/zImage --ramdisk $PACKAGEDIR/ramdisk.gz --base 0x80200000 --pagesize 2048 --ramdiskaddr 0x81500000 --output $PACKAGEDIR/boot.img 
-
+export curdate=`date "+%m-%d-%Y"`
+cd $PACKAGEDIR
+cp -R ../META-INF .
+rm ramdisk.gz
+rm zImage
+rm ../KT747-Kernel-TW-ICS*.zip
+zip -r ../KT747-Kernel-TW-ICS-$curdate.zip .
+cd $KERNELDIR
