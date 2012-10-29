4452865b5a8b551ed7c3aee476f1bfdb248134c3
ktoonsez <ktoonsez@gmail.com>
2012-10-28 18:52:36 -0700

    update build all branches script

--- build_all_branches.sh
+++ build_all_branches.sh
@@ -39,6 +39,11 @@ make clean
 echo "Make Mrproper"
 make mrproper
 ./build_d2att_sammy.sh
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_d2spr_sammy.sh
 
 echo "Switch to JellyBean"
 git checkout jellybean
