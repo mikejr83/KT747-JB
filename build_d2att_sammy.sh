c5f31ccb6359f6abbef161e9a2e19ec4306f10aa
ktoonsez <ktoonsez@gmail.com>
2012-12-12 18:29:00 -0700

    fix typos in build scripts

--- build_d2att_sammy.sh
+++ build_d2att_sammy.sh
@@ -68,7 +68,7 @@ if [ -e $KERNELDIR/arch/arm/boot/zImage ]; then
 	./mkbootimg --cmdline 'console = null androidboot.hardware=qcom user_debug = 31' --kernel $PACKAGEDIR/zImage --ramdisk $PACKAGEDIR/ramdisk.gz --base 0x80200000 --pagesize 2048 --ramdiskaddr 0x81500000 --output $PACKAGEDIR/boot.img 
 	export curdate=`date "+%m-%d-%Y"`
 	cd $PACKAGEDIR
-	cp -R ../META-INF_ICS META-INF
+	cp -R ../META-INF META-INF
 	rm ramdisk.gz
 	rm zImage
 	rm ../KT747-Kernel-TW-ICS*.zip
