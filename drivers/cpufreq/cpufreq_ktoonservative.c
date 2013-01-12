d48edca7df2559b46c2ebc0138a7665fb2216fcd
ktoonsez <ktoonsez@gmail.com>
2013-01-11 17:56:45 -0700

    cpufreq: ktoonservative block cpu when current Mhz is at minimum

--- drivers/cpufreq/cpufreq_ktoonservative.c
+++ drivers/cpufreq/cpufreq_ktoonservative.c
@@ -450,7 +450,7 @@ static void dbs_check_cpu(struct cpu_dbs_info_s *this_dbs_info)
 
 	/* Check for load increase is greater than hotplug value */
 	if (max_load > dbs_tuners_ins.up_threshold_hotplug) {
-		if (num_online_cpus() < 2)
+		if (num_online_cpus() < 2 && policy->cur != policy->min)
 		{
 			if (dbs_tuners_ins.use_yoyo_cpuload)
 			{
