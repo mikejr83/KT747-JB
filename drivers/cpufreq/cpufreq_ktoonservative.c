0697ad29602776dba4d335c575a12b6fd22ab88d
ktoonsez <ktoonsez@gmail.com>
2013-02-06 22:01:37 -0700

    cpufreq: ktoonservative: Add boost_turn_on_2nd_core to tunables

--- drivers/cpufreq/cpufreq_ktoonservative.c
+++ drivers/cpufreq/cpufreq_ktoonservative.c
@@ -105,6 +105,7 @@ static struct dbs_tuners {
 	unsigned int cpu_down_block_cycles;
 	unsigned int ignore_nice;
 	unsigned int no_2nd_cpu_screen_off;
+	unsigned int boost_turn_on_2nd_core;
 	unsigned int use_yoyo_cpuload;
 	unsigned int freq_step;
 } dbs_tuners_ins = {
@@ -116,6 +117,7 @@ static struct dbs_tuners {
 	.sampling_down_factor = DEF_SAMPLING_DOWN_FACTOR,
 	.ignore_nice = 0,
 	.no_2nd_cpu_screen_off = 1,
+	.boost_turn_on_2nd_core = 1,
 	.use_yoyo_cpuload = 0,
 	.freq_step = 5,
 };
@@ -210,6 +212,7 @@ show_one(down_threshold_hotplug, down_threshold_hotplug);
 show_one(cpu_down_block_cycles, cpu_down_block_cycles);
 show_one(ignore_nice_load, ignore_nice);
 show_one(no_2nd_cpu_screen_off, no_2nd_cpu_screen_off);
+show_one(boost_turn_on_2nd_core, boost_turn_on_2nd_core);
 show_one(use_yoyo_cpuload, use_yoyo_cpuload);
 show_one(freq_step, freq_step);
 
@@ -332,6 +335,19 @@ static ssize_t store_no_2nd_cpu_screen_off(struct kobject *a, struct attribute *
 	return count;
 }
 
+static ssize_t store_boost_turn_on_2nd_core(struct kobject *a, struct attribute *b, const char *buf, size_t count)
+{
+	unsigned int input;
+	int ret;
+	ret = sscanf(buf, "%u", &input);
+
+	if (input != 0 && input != 1)
+		input = 0;
+
+	dbs_tuners_ins.boost_turn_on_2nd_core = input;
+	return count;
+}
+
 static ssize_t store_ignore_nice_load(struct kobject *a, struct attribute *b,
 				      const char *buf, size_t count)
 {
@@ -406,6 +422,7 @@ define_one_global_rw(down_threshold_hotplug);
 define_one_global_rw(cpu_down_block_cycles);
 define_one_global_rw(ignore_nice_load);
 define_one_global_rw(no_2nd_cpu_screen_off);
+define_one_global_rw(boost_turn_on_2nd_core);
 define_one_global_rw(freq_step);
 define_one_global_rw(use_yoyo_cpuload);
 
@@ -419,6 +436,7 @@ static struct attribute *dbs_attributes[] = {
 	&down_threshold_hotplug.attr,
 	&cpu_down_block_cycles.attr,
 	&no_2nd_cpu_screen_off.attr,
+	&boost_turn_on_2nd_core.attr,
 	&ignore_nice_load.attr,
 	&freq_step.attr,
 	&use_yoyo_cpuload.attr,
@@ -429,6 +447,12 @@ static struct attribute_group dbs_attr_group = {
 	.attrs = dbs_attributes,
 	.name = "ktoonservative",
 };
+
+void boostpulse_relay_kt()
+{
+	if (num_online_cpus() < 2 && dbs_tuners_ins.boost_turn_on_2nd_core)
+		schedule_work_on(0, &hotplug_online_work);
+}
 
 /************************** sysfs end ************************/
 
