445f8632914dc898a59d7db7c527062676648f5c
ktoonsez <ktoonsez@gmail.com>
2013-03-07 21:17:33 -0700

    cpufreq:ktoonservative: When disable_hotplug_bt_active becomes true immediately turn CPU 1 ON

--- drivers/cpufreq/cpufreq_ktoonservative.c
+++ drivers/cpufreq/cpufreq_ktoonservative.c
@@ -198,7 +198,11 @@ static struct notifier_block dbs_cpufreq_notifier_block = {
 void set_bluetooth_state_kt(bool val)
 {
 	if (val == true && dbs_tuners_ins.disable_hotplug_bt == 1)
+	{
 		disable_hotplug_bt_active = true;
+		if (num_online_cpus() < 2)
+			cpu_up(1);
+	}
 	else
 		disable_hotplug_bt_active = false;
 }
