cb0834f46284f8d4fae480aa44a81ca7d0139fae
ktoonsez <ktoonsez@gmail.com>
2013-03-08 18:12:03 -0700

    Revert "Bluetooth: hidp: use dest address from session info.  While hid device unpair process, we are trying to access released socket as apart of getting hci device which results a kernel panic."
    This reverts commit debc7649181c24d5b533f5885f6368f05200ba75.

--- net/bluetooth_mgmt/hidp/core.c
+++ net/bluetooth_mgmt/hidp/core.c
@@ -777,12 +777,12 @@ static int hidp_session(void *arg)
 
 static struct device *hidp_get_device(struct hidp_session *session)
 {
-	bdaddr_t *dst = &session->bdaddr;
-
+	bdaddr_t *src = &bt_sk(session->ctrl_sock->sk)->src;
+	bdaddr_t *dst = &bt_sk(session->ctrl_sock->sk)->dst;
 	struct device *device = NULL;
 	struct hci_dev *hdev;
 
-	hdev = hci_get_route(dst, BDADDR_ANY);
+	hdev = hci_get_route(dst, src);
 	if (!hdev)
 		return NULL;
 
