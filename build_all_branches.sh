cdd83441809895eda60cbc953c7ef09ab554bd47
ktoonsez <ktoonsez@gmail.com>
2013-03-24 16:02:10 -0700

    Update build all branches

--- build_all_branches.sh
+++ build_all_branches.sh
@@ -1,4 +1,28 @@
-echo "Switch to JellyBean AOSP 4.2"
+echo "Switch to JellyBean AOSP 4.2.2 - 3.4"
+git checkout mr1-3.4
+
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_d2att_aosp.sh
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_d2spr_aosp.sh
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_d2vzw_aosp.sh
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_d2usc_aosp.sh
+
+echo "Switch to JellyBean AOSP 4.2.2 - 3.0"
 git checkout mr1
 
 echo "Make Clean"
@@ -60,8 +84,27 @@ echo "Make Mrproper"
 make mrproper
 ./build_d2att_sammy.sh
 
-echo "Switch to Sammy Jellybean"
+echo "Switch to Sammy Jellybean - 4.1.1"
 git checkout sammy_jb
+
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_d2att_sammy.sh
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_d2spr_sammy.sh
+echo "Make Clean"
+make clean
+echo "Make Mrproper"
+make mrproper
+./build_d2vzw_sammy.sh
+
+echo "Switch to Sammy Jellybean - 4.1.2"
+git checkout sammy_jb_4.1.2
 
 echo "Make Clean"
 make clean
