76905d996738044681d462e6f9c28110225db766
ktoonsez <ktoonsez@gmail.com>
2013-03-26 14:56:50 -0700

    Update build all branches - REMOVE ICS

--- build_all_branches.sh
+++ build_all_branches.sh
@@ -70,20 +70,6 @@ echo "Make Mrproper"
 make mrproper
 ./build_d2usc_aosp.sh
 
-echo "Switch to ICS"
-git checkout ics
-
-echo "Make Clean"
-make clean
-echo "Make Mrproper"
-make mrproper
-./build_d2att_aosp.sh
-echo "Make Clean"
-make clean
-echo "Make Mrproper"
-make mrproper
-./build_d2att_sammy.sh
-
 echo "Switch to Sammy Jellybean - 4.1.1"
 git checkout sammy_jb
 
