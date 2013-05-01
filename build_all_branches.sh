69ddaab74c140daeaeeb42c187cbc1c866e4a436
ktoonsez <ktoonsez@gmail.com>
2013-04-30 17:29:44 -0700

    Update build all branches script

--- build_all_branches.sh
+++ build_all_branches.sh
@@ -46,49 +46,6 @@ echo "Make Mrproper"
 make mrproper
 ./build_d2usc_aosp.sh
 
-echo "Switch to JellyBean AOSP 4.1.2"
-git checkout jellybean
-
-echo "Make Clean"
-make clean
-echo "Make Mrproper"
-make mrproper
-./build_d2att_aosp.sh
-echo "Make Clean"
-make clean
-echo "Make Mrproper"
-make mrproper
-./build_d2spr_aosp.sh
-echo "Make Clean"
-make clean
-echo "Make Mrproper"
-make mrproper
-./build_d2vzw_aosp.sh
-echo "Make Clean"
-make clean
-echo "Make Mrproper"
-make mrproper
-./build_d2usc_aosp.sh
-
-echo "Switch to Sammy Jellybean - 4.1.1"
-git checkout sammy_jb
-
-echo "Make Clean"
-make clean
-echo "Make Mrproper"
-make mrproper
-./build_d2att_sammy.sh
-echo "Make Clean"
-make clean
-echo "Make Mrproper"
-make mrproper
-./build_d2spr_sammy.sh
-echo "Make Clean"
-make clean
-echo "Make Mrproper"
-make mrproper
-./build_d2vzw_sammy.sh
-
 echo "Switch to Sammy Jellybean - 4.1.2"
 git checkout sammy_jb_4.1.2
 
@@ -109,4 +66,4 @@ make mrproper
 ./build_d2vzw_sammy.sh
 
 echo "Switch to JellyBean 4.2"
-git checkout mr1
+git checkout mr1-3.4
