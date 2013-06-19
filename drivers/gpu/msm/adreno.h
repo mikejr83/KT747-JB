ff2f7c4450e0362803291c428ff6abf39d6c18a5
Steve Kondik <shade@chemlab.org>
2013-06-18 19:28:41 -0700

    Merge branch 'next' of git://github.com/CyanogenMod/android_kernel_samsung_d2 into cm-10.1
    Conflicts:
    	arch/arm/mach-msm/board-8064-pmic.c
    	arch/arm/mach-msm/board-8930-pmic.c
    	arch/arm/mach-msm/board-8960-pmic.c
    	drivers/gpu/msm/adreno.h
    	drivers/media/video/msm/vfe/msm_vfe32.c
    	drivers/video/msm/vidc/common/dec/vdec.c
    	include/linux/wcnss_wlan.h

--- drivers/gpu/msm/adreno.h
+++ drivers/gpu/msm/adreno.h
@@ -111,7 +111,13 @@ struct adreno_device {
 	unsigned int gpulist_index;
 	struct ocmem_buf *ocmem_hdl;
 	unsigned int ocmem_base;
+<<<<<<< HEAD
 	unsigned int gpu_cycles;
+=======
+	struct kgsl_memdesc on_resume_cmd;
+	unsigned int on_resume_ib[3];
+	bool on_resume_issueib;
+>>>>>>> c173abf... Merge branch 'next' of git://github.com/CyanogenMod/android_kernel_samsung_d2 into cm-10.1
 };
 
 struct adreno_gpudev {
