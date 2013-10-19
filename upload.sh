687d79a2101bdaee30fa46ffe20058837fda9174
mikejr83 <mikejr83@gmail.com>
2013-10-18 22:25:04 -0400

    Upload to dropbox on successful deploy

---
+++ upload.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+touch ~/.dropbox_uploader
+
+echo "APPKEY="$APPKEY >> ~/.dropbox_uploader
+echo "APPSECRET="$APPSECRET >> ~/.dropbox_uploader
+echo "ACCESS_LEVEL="$ACCESS_LEVEL >> ~/.dropbox_uploader
+echo "OAUTH_ACCESS_TOKEN="$OAUTH_ACCESS_TOKEN >> ~/.dropbox_uploader
+echo "OAUTH_ACCESS_TOKEN_SECRET="$OAUTH_ACCESS_TOKEN_SECRET >> ~/.dropbox_uploader
+
+chmod +x dropbox_uploader.sh
+
+./dropbox_uploader.sh ../Packages/KT747* Public/
