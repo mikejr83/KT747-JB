08123e4baa37523cb7fd691942c77352a431b557
Luis Cruz <ljc2491@gmail.com>
2014-06-18 14:36:12 -0500

    motor: tspdrv: return on invalid buffer index
    Update from VPUDND8
    
    Change-Id: I941042c68d89ed636d8be9f2f54bcc1c20f930a8
    Signed-off-by: Luis Cruz <ljc2491@gmail.com>

--- drivers/motor/tspdrv.c
+++ drivers/motor/tspdrv.c
@@ -552,6 +552,7 @@ static ssize_t write(struct file *file, const char *buf, size_t count,
 			** (Should never happen).
 			*/
 			DbgOut((KERN_EMERG "tspdrv: invalid buffer index.\n"));
+			return 0;
 		}
 
 		/* Check bit depth */
@@ -572,6 +573,7 @@ static ssize_t write(struct file *file, const char *buf, size_t count,
 			** (Should never happen).
 			*/
 			DbgOut((KERN_EMERG "tspdrv: invalid data size.\n"));
+			return 0;
 		}
 
 		/* Check actuator index */
